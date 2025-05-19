803c0670a7728af18e259304d37a711156bce96c
pascal <pascal@pakab.de>
2025-05-19 23:11:21 +0200

    refactor chord pro rendering

--- app/src/main/java/de/pakab/chordmate/ChordPro.kt
+++ app/src/main/java/de/pakab/chordmate/ChordPro.kt
@@ -2,10 +2,41 @@ package de.pakab.chordmate
 
 import kotlin.math.max
 
-class Chord(
-    val position: Int,
-    val text: StringBuilder,
-)
+private fun findChordPositions(matchResults: List<MatchResult>): List<Int> {
+    var positions: MutableList<Int> = ArrayList<Int>()
+    var accumulator = 0
+    for (mr in matchResults) {
+        positions.add(mr.range.start - accumulator)
+        accumulator += mr.range.last - mr.range.start + 1
+    }
+    return positions
+}
+
+private fun stripChords(
+    line: String,
+    matchResults: List<MatchResult>,
+): String {
+    var strippedLine = line
+    for (mr in matchResults.asReversed()) {
+        strippedLine = strippedLine.replaceRange(mr.range, "")
+    }
+    return strippedLine
+}
+
+private fun assembleChordLine(
+    matchResults: List<MatchResult>,
+    chordPositions: List<Int>,
+): String {
+    val sb = StringBuilder()
+    for ((mr, pos) in (matchResults zip chordPositions)) {
+        val symbol = mr.value.substring(1, mr.value.length - 1)
+        val minSpaceBeforeSymbol = if (pos == 0) 0 else 1
+        val n = max(minSpaceBeforeSymbol, pos - sb.length)
+        sb.append(" ".repeat(n))
+        sb.append(symbol)
+    }
+    return sb.toString()
+}
 
 private fun renderChordProLine(line: String): Array<String> {
     val regex = Regex("""\[.*?\]""")
@@ -14,26 +45,9 @@ private fun renderChordProLine(line: String): Array<String> {
         return arrayOf(line)
     }
 
-    var strippedLine = line
-    for (mr in matchResults.asReversed()) {
-        strippedLine = strippedLine.replaceRange(mr.range, "")
-    }
-    var positions: MutableList<Int> = ArrayList<Int>()
-    var accumulator = 0
-    for (mr in matchResults) {
-        positions.add(mr.range.start - accumulator)
-        accumulator += mr.range.last - mr.range.start + 1
-    }
-
-    val sb = StringBuilder()
-    for ((mr, pos) in (matchResults zip positions)) {
-        val symbol = mr.value.substring(1, mr.value.length - 1)
-        val minSpaceBeforeSymbol = if (pos == 0) 0 else 1
-        val n = max(minSpaceBeforeSymbol, pos - sb.length)
-        sb.append(" ".repeat(n))
-        sb.append(symbol)
-    }
-    return arrayOf(sb.toString(), strippedLine)
+    val chordLine = assembleChordLine(matchResults, findChordPositions(matchResults))
+    val textLine = stripChords(line, matchResults)
+    return arrayOf(chordLine, textLine)
 }
 
 fun renderChordPro(code: String?): String {
