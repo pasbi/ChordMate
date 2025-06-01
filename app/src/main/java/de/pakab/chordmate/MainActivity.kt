b80c645db88de7b0817635a391e41c24e6337197
pascal <pascal@pakab.de>
2025-06-01 10:14:40 +0200

    extract spotify stuff into SpotifyControl

--- app/src/main/java/de/pakab/chordmate/MainActivity.kt
+++ app/src/main/java/de/pakab/chordmate/MainActivity.kt
@@ -2,68 +2,23 @@ package de.pakab.chordmate
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupActionBarWithNavController
-import com.spotify.android.appremote.api.ConnectionParams
-import com.spotify.android.appremote.api.Connector
-import com.spotify.android.appremote.api.SpotifyAppRemote
-import com.spotify.protocol.types.Track
-import com.spotify.sdk.android.auth.AuthorizationClient
-import com.spotify.sdk.android.auth.AuthorizationRequest
-import com.spotify.sdk.android.auth.AuthorizationResponse
 import de.pakab.chordmate.databinding.ActivityMainBinding
-
-private var spotifyAppRemote: SpotifyAppRemote? = null
-val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onStart() {
         super.onStart()
-
-        val connectionParams =
-            ConnectionParams
-                .Builder(getString(R.string.SPOTIFY_CLIENT_ID))
-                .setRedirectUri(getString(R.string.SPOTIFY_REDIRECT_URI))
-                .showAuthView(true)
-                .build()
-
-        Log.d(TAG, "Connecting ...")
-        SpotifyAppRemote.connect(
-            this,
-            connectionParams,
-            object : Connector.ConnectionListener {
-                override fun onConnected(appRemote: SpotifyAppRemote?) {
-                    spotifyAppRemote = appRemote
-                    Log.d("MainActivity", "Connected to Spotify.")
-                    connected()
-                }
-
-                override fun onFailure(throwable: Throwable) {
-                    Log.e("MainActivity", throwable.message, throwable)
-                }
-            },
-        )
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val request: AuthorizationRequest =
-            AuthorizationRequest
-                .Builder(
-                    getString(R.string.SPOTIFY_CLIENT_ID),
-                    AuthorizationResponse.Type.TOKEN,
-                    getString(R.string.SPOTIFY_REDIRECT_URI),
-                ).setScopes(arrayOf("app-remote-control"))
-                .build()
-
-        AuthorizationClient.openLoginActivity(this, 1337, request)
-
+        SpotifyControl.start(this)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
@@ -79,45 +34,13 @@ class MainActivity : AppCompatActivity() {
         data: Intent?,
     ) {
         super.onActivityResult(requestCode, resultCode, data)
-
-        if (requestCode == 1337) {
-            val response: AuthorizationResponse = AuthorizationClient.getResponse(resultCode, data)
-
-            when (response.type) {
-                AuthorizationResponse.Type.TOKEN -> {
-                    // do whatever you need with the access token `response.accessToken`
-
-                    // ADD YOUR CODE HERE and it should be able to play !
-                }
-
-                AuthorizationResponse.Type.ERROR -> {
-                    Log.e(TAG, "Auth error : " + response.error)
-                }
-
-                else -> {
-                    Log.e(TAG, "Auth result: " + response.type)
-                }
-            }
-        }
-    }
-
-    private fun connected() {
-        spotifyAppRemote?.let {
-            // Play a playlist
-            val playlistURI = "spotify:playlist:37i9dQZF1DX2sUQwD7tbmL"
-            it.playerApi.play(playlistURI)
-            // Subscribe to PlayerState
-            it.playerApi.subscribeToPlayerState().setEventCallback {
-                val track: Track = it.track
-                Log.d("MainActivity", track.name + " by " + track.artist.name)
-            }
+        if (requestCode == SpotifyControl.requestCode) {
+            SpotifyControl.onRequestReturns(this, resultCode, data)
         }
     }
 
     override fun onStop() {
         super.onStop()
-        spotifyAppRemote?.let {
-            SpotifyAppRemote.disconnect(it)
-        }
+        SpotifyControl.stop()
     }
 }
---
+++ app/src/main/java/de/pakab/chordmate/SpotifyControl.kt
@@ -0,0 +1,112 @@
+package de.pakab.chordmate
+
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import android.widget.Toast
+import com.spotify.android.appremote.api.ConnectionParams
+import com.spotify.android.appremote.api.Connector
+import com.spotify.android.appremote.api.SpotifyAppRemote
+import com.spotify.sdk.android.auth.AuthorizationClient
+import com.spotify.sdk.android.auth.AuthorizationRequest
+import com.spotify.sdk.android.auth.AuthorizationResponse
+
+object SpotifyControl {
+    private val TAG = "SpotifyControl"
+    private var spotifyAppRemote: SpotifyAppRemote? = null
+    val requestCode = 237657
+
+    fun start(activity: MainActivity) {
+        val request: AuthorizationRequest =
+            AuthorizationRequest
+                .Builder(
+                    activity.getString(R.string.SPOTIFY_CLIENT_ID),
+                    AuthorizationResponse.Type.TOKEN,
+                    activity.getString(R.string.SPOTIFY_REDIRECT_URI),
+                ).setScopes(arrayOf("app-remote-control"))
+                .build()
+
+        Log.d(TAG, "open login activity")
+        AuthorizationClient.openLoginActivity(activity, requestCode, request)
+    }
+
+    fun onRequestReturns(
+        context: Context,
+        resultCode: Int,
+        data: Intent?,
+    ) {
+        val response: AuthorizationResponse = AuthorizationClient.getResponse(resultCode, data)
+
+        when (response.type) {
+            AuthorizationResponse.Type.TOKEN -> {
+                Log.d(TAG, "Request returned successfully.")
+                connectToRemote(context)
+            }
+            AuthorizationResponse.Type.ERROR -> {
+                Log.e(TAG, "Auth error : " + response.error)
+            }
+            else -> {
+                Log.e(TAG, "Unexpected Auth result: " + response.type)
+            }
+        }
+    }
+
+    private fun connectToRemote(context: Context) {
+        val connectionParams =
+            ConnectionParams
+                .Builder(context.getString(R.string.SPOTIFY_CLIENT_ID))
+                .setRedirectUri(context.getString(R.string.SPOTIFY_REDIRECT_URI))
+                .showAuthView(true)
+                .build()
+
+        Log.d(TAG, "Connecting to Remote App ...")
+        SpotifyAppRemote.connect(
+            context,
+            connectionParams,
+            object : Connector.ConnectionListener {
+                override fun onConnected(appRemote: SpotifyAppRemote?) {
+                    spotifyAppRemote = appRemote
+                    Log.d(TAG, "Connected to Spotify.")
+                    Toast
+                        .makeText(
+                            context,
+                            context.getString(R.string.connected_to_spotify),
+                            Toast.LENGTH_SHORT,
+                        ).show()
+                    connected()
+                }
+
+                override fun onFailure(throwable: Throwable) {
+                    Log.e(TAG, throwable.message, throwable)
+                    Toast
+                        .makeText(
+                            context,
+                            context.getString(R.string.failed_to_connect_to_spotify),
+                            Toast.LENGTH_SHORT,
+                        ).show()
+                }
+            },
+        )
+    }
+
+    fun stop() {
+        spotifyAppRemote?.let {
+            it.playerApi.pause()
+            SpotifyAppRemote.disconnect(it)
+        }
+    }
+
+    private fun connected() {
+        spotifyAppRemote?.let {
+            it.playerApi.resume()
+//            // Play a playlist
+//            val playlistURI = "spotify:playlist:37i9dQZF1DX2sUQwD7tbmL"
+//            it.playerApi.play(playlistURI)
+//            // Subscribe to PlayerState
+//            it.playerApi.subscribeToPlayerState().setEventCallback {
+//                val track: Track = it.track
+//                Log.d("MainActivity", track.name + " by " + track.artist.name)
+//            }
+        }
+    }
+}
